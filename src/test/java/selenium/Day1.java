3a04bd34bc149e2a0a66ecc36626a596ceca9955
bhagyashree <bhagu020289@gmail.com>
2021-09-08 08:03:09 +0530

    Seleium update

---
+++ src/test/java/selenium/Day1.java
@@ -0,0 +1,15 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Day1 {
+@Test
+	public void Start() {
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\bhagy\\Desktop\\BhagyaGitProjects\\Drivers\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://demoqa.com/");
+    driver.quit();
+	}
+}
