218c307499eb780d656d898e39133a67b273aea3
bhagyashree <bhagu020289@gmail.com>
2021-09-09 05:20:48 +0530

    FInal for today

--- src/test/java/selenium/Day1.java
+++ src/test/java/selenium/Day1.java
@@ -7,8 +7,7 @@ import org.testng.annotations.Test;
 public class Day1 {
 @Test
 	public void Start() {
-	System.out.println("Now atleast this First for today should work");
-	
+	System.out.println("Now atleast this Final for today should work");
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\bhagy\\Desktop\\BhagyaGitProjects\\Drivers\\chromedriver_win32\\chromedriver.exe");
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://demoqa.com/");
